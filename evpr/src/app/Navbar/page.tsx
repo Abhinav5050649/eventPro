a6debed32821433aca48b3752abe485904f0bfd7
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-12-17 00:06:31 +0530

    Add: Navbar, Update: package*.json & src\app\page.tsx

---
+++ evpr/src/app/Navbar/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar() {
+    
+    return (
+        <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+            <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+                <a className="flex-none text-2xl font-semibold dark:text-white" href="/">EventPro</a>
+                <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
+                <a className="font-medium text-2l text-blue-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/login" aria-current="page">Login</a>
+                <a className="font-medium text-2l text-blue-500 hover:text-gray-400 dark:hover:text-gray-600 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/signup">Signup</a>
+                </div>
+            </nav>
+        </header>
+    );
+}
