cf160f6ff63e788a30cd749c09973c66ce6726ea
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-12-18 17:03:34 +0530

    Update: package*.json, login and signup pages ; Add: api folder, dashboard

---
+++ evpr/src/app/dashboard/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { NextResponse } from "next/server";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function ProfilePage(){
+    const router = useRouter();
+    
+    const logout = async() => {
+        try{
+            const response = await axios.get("/api/users/logout");
+            router.push("/");
+        }catch(error: any){
+            console.log(error.message)
+            toast.error(error.message)
+        }
+    }
+
+    return(
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr/>
+            <p>Profile Page</p>
+            <hr/>
+            <button onClick={logout} className="bg-green-700 hover:bg-blue-900 mt-4 text-white font-bold py-2 px-4 rounded">Logout</button>
+        </div>
+    )
+}
--- evpr/src/app/signup/page.tsx
+++ evpr/src/app/signup/page.tsx
@@ -10,28 +10,35 @@ export default function SignupPage() {
     const [user, setUser] = React.useState({
         email: "",
         password: "",
+        secpass: "",
         username: "",
+        organization: false,
     });
 
     const onSignup = async() => {
         try {
-            const response = await axios.post("/api/users/signup", user);
-            console.log("Singup success", response.data);
-            router.push("/login");
+            // if (user.password !== user.secpass) {
+            //     alert("Passwords do not match!");
+            // }
+            // else {
+                const response = await axios.post("/api/users/signup", user);
+                console.log("Signup success", response.data);
+                router.push("/login");
+           // }
         } catch (error: any) {
-            console.log("signup failed", error.message)
+            console.log("Signup failed", error.message)
         }
     }
 
     return (
         <section className="bg-white dark:bg-gray-900">
-            <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-                <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
-                    <img
+            <div className="lg:grid min-h-screen lg:grid-cols-12">
+                <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6 bg-orange-700">
+                    {/* <img
                         alt="Pattern"
                         src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=htmlFormat&fit=crop&w=870&q=80"
                         className="absolute inset-0 h-full w-full object-cover"
-                    />
+                    /> */}
                 </aside>
 
                 <main
@@ -63,6 +70,8 @@ export default function SignupPage() {
                                     type="text"
                                     id="username"
                                     name="username"
+                                    value={user.username}
+                                    onChange={(e) => setUser({...user, username: e.target.value})}
                                     className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
                                 />
                             </div>
@@ -76,6 +85,8 @@ export default function SignupPage() {
                                     type="email"
                                     id="Email"
                                     name="email"
+                                    value={user.email}
+                                    onChange={(e) => setUser({...user, email: e.target.value})}
                                     className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
                                 />
                             </div>
@@ -92,6 +103,8 @@ export default function SignupPage() {
                                     type="password"
                                     id="Password"
                                     name="password"
+                                    value={user.password}
+                                    onChange={(e) => setUser({...user, password: e.target.value})}
                                     className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
                                 />
                             </div>
@@ -108,6 +121,8 @@ export default function SignupPage() {
                                     type="password"
                                     id="PasswordConfirmation"
                                     name="password_confirmation"
+                                    value={user.secpass}
+                                    onChange={(e) => setUser({...user, secpass: e.target.value})}
                                     className="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
                                 />
                             </div>
@@ -118,6 +133,7 @@ export default function SignupPage() {
                                     type="checkbox"
                                     id="OrgBool"
                                     name="OrgBool"
+                                    onClick={(e) => setUser({...user, organization: true})}
                                     className="h-5 w-5 rounded-md border-gray-200 bg-white shadow-sm"
                                 />
 
@@ -130,6 +146,7 @@ export default function SignupPage() {
                             <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
                                 <button
                                     className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 dark:hover:bg-blue-700 dark:hover:text-white"
+                                    onClick={onSignup}
                                 >
                                     Create an account
                                 </button>
